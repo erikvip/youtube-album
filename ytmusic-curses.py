51880640c307e09acf7096adbde3994850845265
Erik Phillips <erikvip@gmail.com>
2015-01-14 23:35:14 -0700

    Cleanup - removed normal curses in favor of urwid widgets

--- ytmusic-curses.py
+++ ytmusic-curses.py
@@ -9,7 +9,6 @@ import os
 import sys
 import pprint
 import logging
-import curses
 import traceback
 import atexit
 # Requires
@@ -17,7 +16,7 @@ import urwid
 import urwid.raw_display
 import urwid.web_display
 
-logging.basicConfig(level=logging.WARNING, format='%(asctime)s %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
 
 def main():
 	# Main handler
@@ -47,6 +46,7 @@ def main():
 #	pile = urwid.Pile([urwid.AttrWrap(urwid.CheckBox(txt),'buttn','buttnf') for txt in text_cb_list])
 #	fil = urwid.Filler(pile)
 	walker = urwid.SimpleListWalker([urwid.AttrMap(urwid.Button(w), 'buttn', 'buttnf') for w in text_cb_list])
+	listbox_content = [
 		#urwid.BoxAdapter(urwid.Filler(urwid.Pile([urwid.AttrWrap(urwid.CheckBox(txt),'buttn','buttnf') for txt in text_cb_list])), 10),
 		urwid.BoxAdapter(
 				urwid.ListBox(
@@ -54,11 +54,12 @@ def main():
 		), 3),
 		urwid.AttrWrap(urwid.Edit("", "wtf", align='left'), 'editbx', 'editfc' ),
 	]
-	logging.info("wtf")
+	
 	head = urwid.Text(text_header)
 	header = urwid.AttrWrap(head, 'header')
 	listbox = urwid.ListBox(urwid.SimpleListWalker(listbox_content))
 	frame = urwid.Frame(urwid.AttrWrap(listbox, 'body'), header=header)	
+
 
 	if urwid.web_display.is_web_request():
 		screen = urwid.web_display.Screen()
@@ -73,7 +74,8 @@ def main():
 		if key == "f6":
 			walker[:] = []
 
-		head.set_text("Key pressed: %s" % key)
+		#head.set_text("Key pressed: %s" % key)
+		
 
 
 
@@ -84,65 +86,9 @@ def main():
 
 
 
-# NCurses compatible logger
-class CursesHandler(logging.Handler):
-	def emit(self, record):
-		win.addstr(1,1,"CURSES HANDLER")
-		pass
-
-# Exit hook function. Clean up curses bindings here
-def terminateCurses():
-	curses.nocbreak()
-	stdscr.keypad(False)
-	curses.echo()
-	curses.endwin()
-
-atexit.register(terminateCurses)
-
-
-ch = CursesHandler()
-logging.getLogger('root').addHandler(ch)
-
 
 
 # main() when invoked from the shell
 if __name__ == '__main__':
+	main()
 
-	# Init curses
-	stdscr = curses.initscr()
-	curses.start_color()
-	curses.use_default_colors()
-	for i in range(0, curses.COLORS):
-		curses.init_pair(i + 1, i, -1)
-
-	curses.cbreak()
-	curses.noecho()
-	stdscr.keypad(1)
-
-	# Main window
-	win = curses.newwin(0,0)
-
-	# Status window
-	y, x = win.getmaxyx()
-
-	# Resize the main window so we have room for status
-	win.resize(y-3 ,x)
-	win.box()
-	win.refresh()
-
-	winstat = curses.newwin(3,x,y-3,0)
-	winstat.box()
-
-	winstat.refresh()
-
-	# Catch exceptions here and output them in curses
-	try:
-		main()
-		time.sleep(5000)
-	except Exception, err:
-		terminateCurses()
- 		traceback.print_exc()
- 		 		
- 		sys.exit(1)
- 
-	time.sleep(5000)
